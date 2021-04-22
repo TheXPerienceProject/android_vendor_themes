f25ea26815d525b3c76038905779e9e120f6d73e
klozz <klozz@TheXPerienceProject.org>
2021-04-22 14:32:20 +0200

    add custom accents
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

---
+++ themes.mk
@@ -0,0 +1,12 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/themes
+
+#Themes Overlays and Accents
+PRODUCT_PACKAGES =+ \
+    AccentColorOneplus \
+    BubblegumPink \
+    CelestialPurple \
+    DreamyPurple \
+    SakuraPink \
+    SpoofyGreen \
+    SpookedPurple
